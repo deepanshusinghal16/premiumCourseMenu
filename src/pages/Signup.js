17f6dc4369521b1621e7df00279aa1245aa33e14
Deepanshu Singhal <deepanshusinghal2003@gmail.com>
2023-03-26 15:19:59 +0530

    Img Changed

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,7 +6,7 @@ import signUpImg from '../assets/signup.png';
 const Signup = ({ setLoggedIn }) => {
   return (
     <div className='bg-black h-[100vh]'>
-      <Template title="Welcome Back" desc1="Build Skill for today, tommorow anmd beyond." desc2="Education to future-proof your career."
+      <Template title="Join us! Thrive." desc1="Embark on a journey of growth and discovery with us." desc2="Unlock your full potential with our education."
         image={signUpImg} fromType="signup" setLoggedIn={setLoggedIn} />
     </div >
   )
